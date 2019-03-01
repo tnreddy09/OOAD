b2dcc574831245d00f6231e3251862b0122bd0fb
AkritiKapur <akritikapur1@gmail.com>
2019-02-28 19:34:57 -0700

    Inventory and section class skeleton

--- Assignment03/inventory.py
+++ Assignment03/inventory.py
@@ -4,8 +4,18 @@
 
 
 class Inventory:
-    def __init__(self):
-        pass
+    def __init__(self, categories):
+        self.n_items = 20
+        self.sections = self.allocate_tools(categories)
+
+    def allocate_tools(self, sections):
+        section_dict = {}
+        for section in sections:
+            section_dict[section.category_name] = section
+        return section_dict
+
+    def add_section(self, section):
+        self.sections[section.name] = section
 
 
 class Section:
@@ -20,3 +30,16 @@ class Section:
             tools.append("{} - {}".format(self.category_name, i))
 
         return tools
+
+
+if __name__ == '__main__':
+    # add section
+    sections = []
+    for name, price, n_tools in zip(["Painting", "Concrete", "Plumbing", "Woodwork", "Yardwork"], [50, 10, 8, 15, 16], [2, 6, 3, 4, 5]):
+        section = Section(name, price, n_tools)
+        sections.append(section)
+
+    inventory = Inventory(sections)
+
+    for section in inventory.sections:
+        print(section, inventory.sections[section].price)
